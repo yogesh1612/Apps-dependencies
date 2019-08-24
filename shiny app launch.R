8f5a323eac7359932b3a2a65cec6cc9e4e3077af
yogesh1612 <yogesh1612@users.noreply.github.com>
2019-08-24 15:20:53 +0530

    Add files via upload

---
+++ shiny app launch.R
@@ -0,0 +1,33 @@
+#------------ JSM APP-------#
+
+source("https://raw.githubusercontent.com/sudhir-voleti/jsm-shinyapp/master/server.R")
+runGitHub("jsm-shinyapp","sudhir-voleti")
+
+#------------ Factor An App-----#
+
+source("https://raw.githubusercontent.com/sudhir-voleti/factor-analysis-shinyapp/master/server.R")
+runGitHub("factor-analysis-shinyapp","sudhir-voleti")
+
+
+#-----Basic Text Analysis shiny app-------#
+
+source("https://raw.githubusercontent.com/sudhir-voleti/basic-text-analysis-shinyapp/master/server.R")
+runGitHub("Basic-text-analysis-shinyapp","sudhir-voleti")
+
+
+#--------- Market Segmentation (NOT WORKING)-------#
+source("https://raw.githubusercontent.com/yogesh1612/Marketing_Segmentation/master/server.R")
+runGitHub("Marketing_Segmentation","yogesh1612")
+
+
+#------ Sentiment Analysis App ----- #
+source("https://github.com/yogesh1612/Sentiment-analysis-shiny-app/blob/master/server.R")
+runGitHub("Sentiment-analysis-shiny-app","yogesh1612")
+
+
+#-----------OCR shiny APP---------------------#
+source("https://raw.githubusercontent.com/sudhir-voleti/ocr-shinyapp/master/dependency-ocr-shinyapp.R") # dep...R ._ Raw -> copy url
+runGitHub("ocr-shinyapp", "sudhir-voleti")
+
+#--------------------------------------#
+
